1c4057582393eb20b1e9e9ca2f44674b8e826d90
Carlos Martínez Loyola <cmartinez@simplelabs.io>
2015-08-30 22:08:09 +1000

    Add project test

---
+++ Challenge.Core.Test/Domain/SetupTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+using Challenge.Core.Configuration;
+using Challenge.Core.Domain;
+using MongoDB.Bson;
+
+namespace Challenge.Core.Test.Domain
+{
+    [TestClass]
+    public class SetupTest
+    {
+        [TestMethod]
+        public void Start()
+        {
+            var settings = CManager.Settings;
+            var client = new MongoClient(settings.MongoDBConnection);
+            var db = client.GetDatabase(settings.MongoDBName);
+            var boxes = db.GetCollection<Box>("boxes");
+            //boxes.InsertOneAsync(new Box() { Name = "RMA2", Address = "Test Address" }).Wait();
+
+            var list = boxes.Find(new BsonDocument()).ToListAsync().Result;
+         }
+    }
+}
---
+++ Challenge.Core/Domain/Box.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge.Core.Domain
+{
+    public class Box
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        [BsonIgnoreIfDefault]
+        public string Address { get; set; }
+    }
+}
