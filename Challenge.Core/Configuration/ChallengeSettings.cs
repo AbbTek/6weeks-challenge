eba6298c2b3eb85f7cf32cd1713d1c91e6aa316b
Carlos Martínez Loyola <cmartinez@simplelabs.io>
2015-08-30 17:24:47 +1000

    Lost files

---
+++ Challenge.Core/Configuration/ChallengeSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge.Core.Configuration
+{
+    public class ChallengeSettings : ConfigurationSection
+    {
+        private const string mongoDBConnection = "dbConnection";
+        private const string mongoDBName = "dbName";
+
+        [ConfigurationProperty(mongoDBConnection)]
+        public string MongoDBConnection
+        {
+            get { return (string)this[mongoDBConnection]; }
+        }
+
+        [ConfigurationProperty(mongoDBName)]
+        public string MongoDBName
+        {
+            get { return (string)this[mongoDBName]; }
+        }
+    }
+}
